b0a58a76645f4da37afa0d2852f507a868f7f273
Emre <okyayatilla@hotmail.com>
2025-07-29 02:36:58 +0300

    added global exception handler components

---
+++ src/main/java/com/example/Ecommerce/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.example.Ecommerce.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private String message;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(String message, LocalDateTime timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+}
