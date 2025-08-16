028b60384d0eee7dc8ea52abf39e327f1783af97
Emre <okyayatilla@hotmail.com>
2025-08-17 00:48:10 +0300

    expanded the ResourceNotFoundException even more

--- src/main/java/com/example/Ecommerce/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/Ecommerce/exception/ResourceNotFoundException.java
@@ -1,12 +1,18 @@
 package com.example.Ecommerce.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ResourceNotFoundException extends RuntimeException {
 
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
+    private final String resourceName;
+    private final String fieldName;
+    private final Object fieldValue;
 
-    public ResourceNotFoundException(String message, Throwable cause) {
-        super(message, cause);
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
     }
 }
