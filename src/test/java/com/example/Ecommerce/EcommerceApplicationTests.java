53de5f559149523b68093b2551e381f8d0cb76d7
EmreOkyay <okyayatilla@hotmail.com>
2025-11-14 18:16:33 +0300

    added mockBeans

--- src/test/java/com/example/Ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/example/Ecommerce/EcommerceApplicationTests.java
@@ -1,10 +1,27 @@
 package com.example.Ecommerce;
 
+import com.example.Ecommerce.order.OrderProducer;
+import com.example.Ecommerce.redis.CacheUpdatePublisher;
+import com.example.Ecommerce.redis.RedisCacheHelper;
+import com.example.Ecommerce.registration.user.UserProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class EcommerceApplicationTests {
+
+	@MockBean
+	private RedisCacheHelper redisCacheHelper;
+
+	@MockBean
+	private CacheUpdatePublisher cachePublisher;
+
+	@MockBean
+	private OrderProducer orderProducer;
+
+	@MockBean
+	private UserProducer userProducer;
 
 	@Test
 	void contextLoads() {
