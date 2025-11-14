8bb70a0b8e1bd5b94fa8cabda0163297cbdf3d36
EmreOkyay <okyayatilla@hotmail.com>
2025-11-15 00:47:39 +0300

    removed unnecessary mocking

--- src/test/java/com/example/Ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/example/Ecommerce/EcommerceApplicationTests.java
@@ -1,29 +1,12 @@
 package com.example.Ecommerce;
 
-import com.example.Ecommerce.order.OrderProducer;
-import com.example.Ecommerce.redis.CacheUpdatePublisher;
-import com.example.Ecommerce.redis.RedisCacheHelper;
-import com.example.Ecommerce.registration.user.UserProducer;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 @Disabled("Skipping contextLoads on CI")
 class EcommerceApplicationTests {
-
-	@MockBean
-	private RedisCacheHelper redisCacheHelper;
-
-	@MockBean
-	private CacheUpdatePublisher cachePublisher;
-
-	@MockBean
-	private OrderProducer orderProducer;
-
-	@MockBean
-	private UserProducer userProducer;
 
 	@Test
 	void contextLoads() {
